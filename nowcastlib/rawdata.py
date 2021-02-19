c65a3f25a44850c804342588bc746d285df105b9
Giulio Starace <giulio.starace@gmail.com>
2021-02-19 21:40:03 +0100

    generate docs html
    Co-authored-by: Giulio Starace <giulio.starace@partner.eso.org>

--- nowcastlib/rawdata.py
+++ nowcastlib/rawdata.py
@@ -6,17 +6,23 @@ import numpy as np
 
 
 def compute_trig_fields(input_df, fields_to_compute=[]):
-    """
-    For a time series pandas dataframe, computes the Cosine and Sine of the seconds of the
+    """For a time series pandas dataframe, computes the Cosine and Sine of the seconds of the
     day of each data point. Also optionally computes the Cosine and Sine equivalent
     additional fields if requested. The computed fields are added to the dataframe which
     is returned with the new fields included.
 
-    :param pandas.core.frame.DataFrane input_df: The dataframe to process
-    :param list[string] fields_to_compute: default []. List of the names of the additional
-        fields for which we wish to compute Cosine and Sine equivalents
-    :return: The original dataframe with the addition of the newly computed fields
-    :rtype: pandas.core.frame.DataFrame
+    Parameters
+    ----------
+    input_df : pandas.core.frame.DataFrame
+        The dataframe to process
+    fields_to_compute : list of string, default []
+        List of the names of the additional fields for which we wish to compute Cosine
+        and Sine equivalents
+
+    Returns
+    -------
+    pandas.core.frame.DataFrame
+        The original dataframe with the addition of the newly computed fields
     """
     # get at which second of the day each data point occured
     datetime = input_df.index.to_series()
@@ -38,12 +44,25 @@ def compute_trig_fields(input_df, fields_to_compute=[]):
 
 
 def bfill_nan(input_array):
-    """
-    Backward-fills NaNs in array
-    Consider the array        [NaN, NaN, 4, 0, 0, NaN, 7, 0]
-    The function will out put [4, 4, 4, 0, 0, 7, 7, 0]
+    """Backward-fills NaNs in numpy array
 
-    Note, NaNs at the end of the array will remain untouched
+    Parameters
+    ----------
+    input_array : numpy.ndarray
+
+    Returns
+    -------
+    numpy.ndarray
+
+    Notes
+    -----
+    NaNs at the end of the array will remain untouched
+
+    Examples
+    --------
+    >>> example_array = np.array([np.nan, np.nan, 4, 0, 0, np.nan, 7, 0])
+    >>> bfill_nan(example_array)
+    array([4., 4., 4., 0., 0., 7., 7., 0.])
     """
     mask = np.isnan(input_array)
     # get index array, but mark the NaNs with a very large number
@@ -54,18 +73,27 @@ def bfill_nan(input_array):
     return input_array[idx]
 
 
-# https://stackoverflow.com/a/54512613/9889508
 def compute_large_gap_mask(data_array, max_gap):
-    """
-    Computes a mask (boolean NumPy array) outlining where there aren't large gaps in the
-    data, as defined by `max_gap`
+    """Computes a mask (boolean NumPy array) outlining where there aren't large gaps
+    in the data, as defined by `max_gap`
 
-    :param numpy.ndarray data_array: The array on which we want to compute the mask
-    :param int max_gap: the maximum number of consecutive NaNs before defining the
-                        section to be a large gap
-    :return: The mask, a boolean numpy.ndarray, where False indicates that the current
-             index is part of a large gap.
-    :rtype: numpy.ndarray
+    Parameters
+    ----------
+    data_array : numpy.ndarray
+        The array on which we want to compute the mask
+    max_gap : int
+        the maximum number of consecutive NaNs before defining the section to be a
+        large gap
+
+    Returns
+    -------
+    numpy.ndarray
+        The mask, a boolean numpy.ndarray, where False indicates that the current
+        index is part of a large gap.
+
+    Notes
+    -----
+    This is an adaption of [this StackOverflow post](https://stackoverflow.com/a/54512613/9889508)
     """
     # where are the NaNs?
     isnan = np.isnan(data_array)
@@ -90,27 +118,36 @@ def compute_large_gap_mask(data_array, max_gap):
 
 
 def compute_dataframe_mask(input_df, max_gap, additional_cols_n=0, column_names=None):
-    """
-    Computes a mask (numpy.ndarray with dtype=boolean) shaped like `data_df` outlining
+    """Computes a mask (numpy.ndarray with dtype=boolean) shaped like `input_df` outlining
     where _all_ columns overlap (i.e. are not NaN), ignoring data gaps smaller than
     `max_gap`
 
-    :param pandas.core.frame.DataFrame input_df: The dataframe upon which to compute the
-        mask
-    :param int max_gap: The maximum number of consecutive NaNs that we ignore before
-        considering this a gap
-    :param int additional_cols_n: default 0. The number of additional columns that may
-        be computed before applying the mask, and therefore need to be considered such
-        that the mask shape matches the (future) dataframe shape
-    :param list[string] column_names: default None. The list of column names to use for
-        checking overlap. If not specified, all columns will be checked. It is highly
-        recommended to specify `column_names` if your dataframe is made of multiple data
-        sources each with multiple columns. In this case you only need to check one column
-        from each data source
+    Parameters
+    ----------
+    input_df : pandas.core.frame.DataFrame
+        The dataframe upon which to compute the mask
+    max_gap : int
+        The maximum number of consecutive NaNs that we ignore before considering this
+        a gap
+    additional_cols_n : int, default 0
+        The number of additional columns that may be computed before applying the mask,
+        and therefore need to be considered such that the mask shape matches the
+        (future) dataframe shape
+    column_names : list of string, default None
+        The list of column names to use for checking overlap. If not specified, all
+        columns will be checked.
 
-    :return: 2-dimensional (tiled) numpy array of the same shape as data_df, to be used as
+    Returns
+    -------
+    numpy.ndarray
+        2-dimensional (tiled) numpy array of the same shape as data_df, to be used as
         an argument to pandas.core.frame.DataFrame.where() or .mask()
-    :rtype: numpy.ndarray
+
+    Notes
+    -----
+    It is highly recommended to specify `column_names` if your dataframe is made of
+    multiple data sources each with multiple columns. In this case you only need to
+    check one column from each data source
     """
     if column_names is None:
         column_names = input_df.columns
@@ -129,16 +166,21 @@ def compute_dataframe_mask(input_df, max_gap, additional_cols_n=0, column_names=
 
 
 def make_chunks(input_df, min_length):
-    """
-    Given a sparse pandas DataFrame (i.e. data interrupted by NaNs), splits the
+    """Given a sparse pandas DataFrame (i.e. data interrupted by NaNs), splits the
     DataFrame into the non-sparse chunks.
 
-    :param pandas.core.frame.DataFrame input_df: the sparse dataframe we wish to process
-    :param int min_length: the minimum length a portion of data must be to be
-        considered a chunk
-    :return: A list, where each element is a DataFrame corresponding to a chunk of the
+    Parameters
+    ----------
+    input_df : pandas.core.frame.DataFrame
+        The sparse dataframe we wish to process
+    min_length : int
+        The minimum length a portion of data must be to be considered a chunk
+
+    Returns
+    -------
+    list of pandas.core.frame.DataFrame
+        A list, where each element is a DataFrame corresponding to a chunk of the
         original
-    :rtype: list[pandas.core.frame.DataFrame]
     """
     sparse_ts = input_df.iloc[:, 0].astype(pd.SparseDtype("float"))
     # extract block length and locations
