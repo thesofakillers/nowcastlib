091257a29a6dbf68b9d394285b7068935056747a
Giulio Starace <giulio.starace@gmail.com>
2021-04-07 16:04:36 +0200

    define chunksync cli subcommand
    Co-authored-by: Giulio Starace <giulio.starace@partner.eso.org>

--- nowcastlib/cli/__init__.py
+++ nowcastlib/cli/__init__.py
@@ -4,6 +4,7 @@ Command-Line interfaces for the Nowcast Library
 import argparse
 import configargparse
 from . import triangulate
+from . import chunksync
 
 
 def main():
@@ -14,6 +15,7 @@ def main():
     command_parsers = parser.add_subparsers(dest="command", help="available commands")
 
     triangulate.configure_parser(command_parsers)
+    chunksync.configure_parser(command_parsers)
 
     args = parser.parse_args()
     command = args.command
@@ -21,3 +23,5 @@ def main():
         parser.print_help()
     elif command == "triangulate":
         triangulate.triangulate(args)
+    elif command == "chunksync":
+        chunksync.chunksync(args)
