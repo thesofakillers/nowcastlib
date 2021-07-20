536bdc28e6134f5bfbb92d95895bfc2948be66ef
Giulio Starace <giulio.starace@gmail.com>
2021-07-20 21:31:00 +0200

    start defining a main pipeline process
    Co-authored-by: Giulio Starace <giulio.starace@partner.eso.org>

--- nowcastlib/pipeline/__init__.py
+++ nowcastlib/pipeline/__init__.py
@@ -2,6 +2,33 @@
 Data processing and Model evaluation pipeline for the Nowcast Library
 .. include:: PIPELINE.md
 """
+from . import structs
 from . import preprocess
 from . import sync
-from . import structs
+from . import split
+from . import postprocess
+
+
+def pipe_dataset(config: structs.DataSet):
+    """
+    Runs all configured steps of the Nowcast Library Pipeline
+    on a set of data described by the input DataSet instance
+    """
+    # preprocessing
+    preprocessed_dfs = preprocess.preprocess_dataset(config)
+    # syncing
+    chunked_df, chunk_locs = sync.synchronize_dataset(config, preprocessed_dfs)
+    # splitting
+    # outer train and test split
+    if config.eval_options is not None:
+        train_data, test_data = split.train_test_split_sparse(
+            chunked_df, config.eval_options, chunk_locs
+        )
+    # # inner train and validation split(s)
+    # val_train, val_test = split.rep_holdout_split(train_data[0], config)
+    # # postprocessing
+    # proc_val_train, proc_val_test = postprocess.postprocess_dataset(
+    #     config,
+    #     val_train[0],
+    #     val_test_data[0],
+    # )
