51e865fae2c3f0e7ebc936a2bb860855d42145a6
Giulio Starace <giulio.starace@gmail.com>
2021-08-01 19:33:07 +0200

    define postprocess_dataset function for splitting and postprocessing a synced_df
    Co-authored-by: Giulio Starace <giulio.starace@partner.eso.org>

--- nowcastlib/pipeline/__init__.py
+++ nowcastlib/pipeline/__init__.py
@@ -4,7 +4,6 @@ Data processing and Model evaluation pipeline for the Nowcast Library
 """
 import pathlib
 import pandas as pd
-from nowcastlib.datasets import serialize_as_chunks
 from . import structs
 from . import process
 from . import sync
@@ -20,49 +19,6 @@ def pipe_dataset(config: structs.DataSet):
     preprocessed_dfs = process.preprocess.preprocess_dataset(config)
     # syncing
     chunked_df, chunk_locs = sync.synchronize_dataset(config, preprocessed_dfs)
-    # splitting
+    # splitting and postprocessing
     if config.split_options is not None:
-        # outer train and test split
-        (train_data, _), (test_data, _) = split.train_test_split_sparse(
-            chunked_df, config.split_options, chunk_locs
-        )
-        # postprocessing
-        [proc_train_data], [proc_test_data] = process.postprocess.postprocess_splits(
-            config, [train_data], [test_data]
-        )
-        # inner train and validation split(s)
-        train_dfs, val_dfs = split.rep_holdout_split_sparse(
-            train_data, config.split_options.validation
-        )
-        # postprocessing
-        (
-            proc_val_train_dfs,
-            proc_val_test_dfs,
-        ) = process.postprocess.postprocess_splits(config, train_dfs, val_dfs)
-        # serialize
-        if config.split_options.output_options is not None:
-            serialize_splits(
-                config.split_options.output_options,
-                proc_train_data,
-                proc_test_data,
-                proc_val_train_dfs,
-                proc_val_test_dfs,
-            )
-
-
-def serialize_splits(config, train_data, test_data, val_train_dfs, val_test_dfs):
-    """
-    Creates directory structure and serializes
-    the dataframes as chunks to hdf5 files
-    """
-    parent_dir = pathlib.Path(config.parent_path)
-    parent_dir.mkdir(parents=config.create_parents, exist_ok=config.overwrite)
-    main_split = parent_dir / "main_split"
-    main_split.mkdir(parents=config.create_parents, exist_ok=config.overwrite)
-    serialize_as_chunks(train_data, main_split / "train_data.hdf5")
-    serialize_as_chunks(test_data, main_split / "test_data.hdf5")
-    cv_split = parent_dir / "cv_split"
-    cv_split.mkdir(parents=config.create_parents, exist_ok=config.overwrite)
-    for i, (train_df, test_df) in enumerate(val_train_dfs, val_test_dfs):
-        serialize_as_chunks(train_df, cv_split / "train_data_{}.hdf5".format(i))
-        serialize_as_chunks(test_df, cv_split / "val_data_{}.hdf5".format(i))
+        process.postprocess.postprocess_dataset(config, (chunked_df, chunk_locs))
--- nowcastlib/pipeline/process/postprocess/__init__.py
+++ nowcastlib/pipeline/process/postprocess/__init__.py
@@ -3,14 +3,18 @@ Functions for post-processing of data
 """
 import sys
 import logging
+import pathlib
 from typing import Optional, List, Tuple
 import numpy as np
 import pandas as pd
 import attr
 import sklearn.preprocessing as sklearn_pproc
 import matplotlib.pyplot as plt
+from nowcastlib.datasets import serialize_as_chunks
 from nowcastlib.pipeline import structs
 from nowcastlib.pipeline import utils
+from nowcastlib.pipeline import sync
+from nowcastlib.pipeline import split
 from nowcastlib.pipeline.process import utils as process_utils
 from . import generate
 
@@ -174,7 +178,7 @@ def postprocess_splits(
     test_dfs: List[pd.core.frame.DataFrame],
 ):
     """
-    Postprocesses a set of data sources given options outlined
+    Postprocesses a set of train-test splits given options outlined
     in the input DataSet config instance.
     """
     logger.info("Postprocessing dataset...")
@@ -226,3 +230,67 @@ def postprocess_splits(
                     field.std_options,
                 )
         return train_dfs, test_dfs
+
+
+def serialize_splits(config, train_data, test_data, val_train_dfs, val_test_dfs):
+    """
+    Creates directory structure and serializes
+    the dataframes as chunks to hdf5 files
+    """
+    parent_dir = pathlib.Path(config.parent_path)
+    parent_dir.mkdir(parents=config.create_parents, exist_ok=config.overwrite)
+    main_split = parent_dir / "main_split"
+    main_split.mkdir(parents=config.create_parents, exist_ok=config.overwrite)
+    serialize_as_chunks(train_data, main_split / "train_data.hdf5")
+    serialize_as_chunks(test_data, main_split / "test_data.hdf5")
+    cv_split = parent_dir / "cv_split"
+    cv_split.mkdir(parents=config.create_parents, exist_ok=config.overwrite)
+    for i, (train_df, test_df) in enumerate(val_train_dfs, val_test_dfs):
+        serialize_as_chunks(train_df, cv_split / "train_data_{}.hdf5".format(i))
+        serialize_as_chunks(test_df, cv_split / "val_data_{}.hdf5".format(i))
+
+
+def postprocess_dataset(
+    config: structs.DataSet,
+    synced_data: Optional[Tuple[pd.core.frame.DataFrame, np.ndarray]] = None,
+):
+    """
+    Postprocesses a dataset given options outlined
+    in the input DataSet config instance.
+    """
+    assert (
+        config.split_options is not None
+    ), "`config.split_options` must be defined to perform postprocessing"
+    # avoid syncing if synced info (dataframe and block locs) are provided
+    if synced_data is None:
+        chunked_df, chunk_locs = sync.synchronize_dataset(config)
+    else:
+        chunked_df = synced_data[0].copy()
+        chunk_locs = synced_data[1].copy()
+
+    # outer train and test split
+    (train_data, _), (test_data, _) = split.train_test_split_sparse(
+        chunked_df, config.split_options, chunk_locs
+    )
+    # postprocessing
+    [proc_train_data], [proc_test_data] = postprocess_splits(
+        config, [train_data], [test_data]
+    )
+    # inner train and validation split(s)
+    train_dfs, val_dfs = split.rep_holdout_split_sparse(
+        train_data, config.split_options.validation
+    )
+    # postprocessing
+    (
+        proc_val_train_dfs,
+        proc_val_test_dfs,
+    ) = postprocess_splits(config, train_dfs, val_dfs)
+    # serialize
+    if config.split_options.output_options is not None:
+        serialize_splits(
+            config.split_options.output_options,
+            proc_train_data,
+            proc_test_data,
+            proc_val_train_dfs,
+            proc_val_test_dfs,
+        )
