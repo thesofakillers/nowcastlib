12f2c69f2eae3f796037304179214435f70b98be
Giulio Starace <giulio.starace@gmail.com>
2021-08-02 18:31:23 +0200

    function for disambiguating union of int str and float for cattrs
    Co-authored-by: Giulio Starace <giulio.starace@partner.eso.org>

--- nowcastlib/pipeline/cli.py
+++ nowcastlib/pipeline/cli.py
@@ -2,6 +2,7 @@
 Command-Line interface functionality for preprocessing
 """
 import json
+from typing import Union, Any, Type
 import argparse
 import cattr
 from nowcastlib.pipeline import structs
@@ -32,5 +33,20 @@ def run_datapipe(args):
     with open(args.config) as json_file:
         config = json.load(json_file)
     cattr_cnvrtr = cattr.GenConverter(forbid_extra_keys=True)
+    cattr_cnvrtr.register_structure_hook(
+        Union[int, float, str], _disambiguate_intfloatstr
+    )
     dataset_config = cattr_cnvrtr.structure(config, structs.DataSet)
     return pipeline.pipe_dataset(dataset_config)
+
+
+def _disambiguate_intfloatstr(train_split: Any, _klass: Type) -> Union[int, float, str]:
+    """Disambiguates Union of int, float and str for cattrs"""
+    if isinstance(train_split, int):
+        return int(train_split)
+    elif isinstance(train_split, float):
+        return float(train_split)
+    elif isinstance(train_split, str):
+        return str(train_split)
+    else:
+        raise ValueError("Cannot disambiguate Union[int, flaot, str]")
